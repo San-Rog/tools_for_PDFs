4b83dce37bf16957dd243d2277889cc6aa4f7297
San-Rog <118987989+San-Rog@users.noreply.github.com>
2025-09-12 06:48:35 -0300

    Add files via upload

---
+++ pdfInitial.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+def main():
+    with st.expander(label='Sobre este aplicativo', expanded=False, icon="📍"):
+        st.write('mmmm')
+
+if __name__ == '__main__':
+    main()
