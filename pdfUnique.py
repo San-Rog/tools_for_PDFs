2c0c5b57f6e4b51abace77846a99411ad60fd2d8
San-Rog <118987989+San-Rog@users.noreply.github.com>
2025-09-12 06:51:24 -0300

    Update pdfUnique.py

--- pdfUnique.py
+++ pdfUnique.py
@@ -1271,7 +1271,8 @@ if __name__ == '__main__':
         st.session_state[keySlides] = []
     st.cache_data.clear() 
     iniFinally(0)
-    with open(r'C:\Users\ACER\Documents\requirements.txt') as f:
+    with open('configuration.css') as f:
         css = f.read()
     st.markdown(f'<style>{css}</style>', unsafe_allow_html=True) 
     main()
+
