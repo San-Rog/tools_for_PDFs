24bdcd4b7d1f4eeff03b21b829c8457158899900
San-Rog <118987989+San-Rog@users.noreply.github.com>
2026-02-21 09:52:38 -0300

    Update pdfInitial.py

--- pdfInitial.py
+++ pdfInitial.py
@@ -2,11 +2,12 @@ import streamlit as st
 
 def main():
     with st.expander(label='Sobre este aplicativo', expanded=False, icon="📍"):
-        st.write('mmmm')
+        st.write('Esta ferramenta trabalha basicamente com pdf simples.')
 
 if __name__ == '__main__':
     with open('configuration.css') as f:
         css = f.read()
     st.markdown(f'<style>{css}</style>', unsafe_allow_html=True) 
     main()
+
 
