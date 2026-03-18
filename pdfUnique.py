55fc04a6182dcb869f12c677c6da796bd55d3c92
San-Rog <118987989+San-Rog@users.noreply.github.com>
2026-03-18 15:23:37 -0300

    Update pdfUnique.py

--- pdfUnique.py
+++ pdfUnique.py
@@ -897,7 +897,6 @@ def main():
         uploadPdf = st.file_uploader('Selecionar arquivo PDF', 
                                      type=['pdf'], 
                                      accept_multiple_files=False,
-                                     max_upload_size=1024*20, 
                                      label_visibility="collapsed")
         if uploadPdf is not None:
             number = 4
